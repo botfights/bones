6f11fff3002159ced71fce353b9d0ee392b65a6f
Colin M. Saunders <colinmsaunders@gmail.com>
2016-09-03 14:44:38 -0700

    some stuff

--- bones.py
+++ bones.py
@@ -8,187 +8,294 @@ from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE, SIG_DFL)
 
 g_catch_exceptions = False
-g_max_time = MAX_TIME
 
 
-class Player():
-    pass
+NORTH   = 1
+EAST    = 2
+SOUTH   = 3
+WEST    = 4
+
+NORTHSOUTH  = 1
+EASTWEST    = 0
+
+# 64 44 42 43 61 14 45:44
+
+def new_tiles():
+    a = []
+    for i in range(7):
+        for j in range(i + 1):
+            a.append((i, j))
+    return a
 
 
-def call_player(player, args):
-    global g_catch_exceptions
-    result = None
-    start = time.clock()
-    try:
-        result = player.play(*args)
-    except KeyboardInterrupt:
-        raise
-    except:
-        logging.warn('caught exception "%s" calling %s (%s)'
-                     % (sys.exc_info()[1], player.player_id, player.playername))
-        if not g_catch_exceptions:
-            raise
-    elapsed = time.clock() - start
-    player.elapsed += elapsed
-    player.calls += 1
-    return result
+def wash_tiles(tiles):
+    random.shuffle(tiles)
+    return tiles
 
 
-def make_player(player_id, dirname):
-    global g_catch_exceptions
-    m = None
-    try:
-        name = 'bot'
-        (f, filename, data) = imp.find_module(name, [dirname, ])
-        m = imp.load_module(name, f, filename, data)
-    except:
-        logging.error('caught exception "%s" loading %s' % \
-                      (sys.exc_info()[1], dirname))
-        if not g_catch_exceptions:
-            raise
-    p = Player()
-    p.player_id = player_id
-    p.playername = dirname
-    z = p.playername.rfind('/')
-    if -1 != z:
-        p.playername = p.playername[z + 1:]
-    p.play = None
-    if None == m or not hasattr(m, 'play'):
-        logging.error('%s has no function "play"; ignoring ...' % dirname)
+def serialize_hand(hand):
+    s = []
+    for a in hand:
+        s.append('%d%d' % (a[0], a[1]))
+    return ' '.join(s)
+
+
+def serialize_table(table):
+    s = []
+    for i in table:
+        a, b = i
+        if b == None:
+            s.append('%d%d' % (a[0], a[1]))
+        else:
+            s.append('%d%d:%d%d' % (a[0], a[1], b[0], b[1]))
+    return ' '.join(s)
+
+
+def deserialize_hand(s):
+    hand = []
+    for i in s.split():
+        a = (int(i[0]), int(i[1]))
+        if a[0] < a[1]:
+            a = (a[1], a[0])
+        hand.append(a)
+    return hand
+
+
+def deserialize_table(s):
+    ends = {}
+    table = []
+    for i in s.split():
+        j = i.split(':')
+        a = (int(j[0][0]), int(j[0][1]))
+        if a[0] < a[1]:
+            a = (a[1], a[0])
+        if 2 == len(j):
+            b = (int(j[1][0]), int(j[1][1]))
+            if b[0] < b[1]:
+                b = (b[1], b[0])
+        if 0 == len(table):
+            b = None
+        else:
+            for end, playable in ends.items():
+                if (playable & 1) and end[0] in (a[0], a[1]):
+                    b = end
+                    break
+                if (playable & 2) and end[1] in (a[0], a[1]):
+                    b = end
+                    break
+                if (playable & 4) and end[0] in (a[0], a[1]):
+                    b = end
+                    break
+                if (playable & 8) and end[0] in (a[0], a[1]):
+                    b = end
+                    break
+            if None == b:
+                raise Exception('invalid play in deserializing "%s"' % s)
+
+        # first play?
+        #
+        if None == b:
+            ends[a] = 3
+
+        # play on a double?
+        #
+        elif b[0] == b[1]:
+            ends[b] = {3:2, 2:8, 8:4, 4:0}[ends[b]]
+            if a[0] == b[0]:
+                ends[a] = 2
+            else:
+                ends[a] = 1
+
+        # high on high
+        #
+        elif a[0] == b[0]:
+            ends[b] &= 2
+            ends[a] = 2
+
+        # high on low
+        #
+        elif a[0] == b[1]:
+            ends[b] &= 1
+            ends[a] = 2
+
+        # low on high
+        #
+        elif a[1] == b[0]:
+            ends[b] &= 2
+            ends[a] = 2
+
+        # low on low
+        #
+        elif a[1] == b[1]:
+            ends[b] &= 1
+            ends[a] = 1
+
+        table.append((a, b))
+    return table
+
+
+def get_ends(table):
+    ends = {}
+    for i in table:
+        a, b = i
+
+        # first play?
+        #
+        if None == b:
+            ends[a] = 3
+
+        # play on a double?
+        #
+        elif b[0] == b[1]:
+            ends[b] = {3:2, 2:8, 8:4, 4:0}[ends[b]]
+            if a[0] == b[0]:
+                ends[a] = 2
+            else:
+                ends[a] = 1
+
+        # high on high
+        #
+        elif a[0] == b[0]:
+            ends[b] &= 2
+            ends[a] = 2
+
+        # high on low
+        #
+        elif a[0] == b[1]:
+            ends[b] &= 1
+            ends[a] = 2
+
+        # low on high
+        #
+        elif a[1] == b[0]:
+            ends[b] &= 2
+            ends[a] = 2
+
+        # low on low
+        #
+        elif a[1] == b[1]:
+            ends[b] &= 1
+            ends[a] = 1
+
+    return ends
+
+
+def get_pip_ends(ends):
+    pip_ends = [{}, {}, {}, {}, {}, {}, {}]
+    for end, playable in ends.items():
+        if 0 == playable:
+            continue
+        if end[0] == end[1]:
+            pip_ends[end[0]][end] = playable
+        else:
+            if 1 == playable:
+                pip_ends[end[0]][end] = playable
+            if 2 == playable:
+                pip_ends[end[1]][end] = playable
+    return pip_ends
+
+
+def get_plays(ends, pip_ends, hand):
+    plays = []
+    if 0 == len(ends):
+        for i in hand:
+            plays.append((i, None))
     else:
-        p.play = getattr(m, 'play')
-    p.elapsed = 0.0
-    p.calls = 0
-    p.get_play = lambda x: call_player(p, (p.player_id, p.hand, x))
-    return p
+        for i in hand:
+            for end, playable in pip_ends[i[0]].items():
+                plays.append((i, end))
+            if i[0] != i[1]:
+                for end, playable in pip_ends[i[1]].items():
+                    plays.append((i, end))
+    return plays
 
 
-def play_game(players):
-    pass
-
-def play_tournament(games, players):
-    """
-    play many games, return map of player_id to wins
-    """
-    for i in players:
-        i.wins = 0
-    for i in range(games):
-        winner.wins += 1
-        t = ''
-        players.sort(key = lambda x : x.wins,reverse = True)
-        for j in players:
-            t += '%s:%d\t' % (j.playername, j.wins)
-        logging.info('BOTFIGHT\t%d\t%d\t%s' % (i, games, t))
-
-
-def usage():
-    print('''\
-bones! see: http://github.com/botfights/bones for dox
-
-usage:
-
-    $ python bones.py <command> [<option> ...] [<arg> ...]
-
-commands:
-
-    human [<opponent>]
-
-                        play against the computer
-
-    game <player1> <player2>
-
-                        play a single game between players
-
-    tournament [<player1>] [<player2>] ...
-
-                        play a tournament between players
-options:
-
-    -h, --help                      show this help
-    --seed=<s>                      set seed for random number generator
-    --catch-exceptions=<off|on>     catch and log exceptions
-    --num-games=<n>                 set number of games for tournament
-    --log-level=<n>                 set log level (10 debug, 20 info, 40 error)
-''')
+def get_count(ends):
+    count = 0
+    for end, playable in ends.items():
+        if end[0] == end[1]:
+            if playable & 3:
+                count += 2 * end[0]
+        else:
+            if playable & 1:
+                count += end[0]
+            if playable & 2:
+                count += end[1]
+    return count
 
 
 def main(argv):
-    if 1 > len(argv):
-        usage()
-        sys.exit()
-    command = argv[0]
-    try:
-        opts, args = getopt.getopt(argv[1:], "h", [
-                                                    "help",
-                                                    "seed=",
-                                                    "catch-exceptions=",
-                                                    "num-games=",
-                                                    "log-level=",
-                                                    ])
-    except getopt.GetoptError as err:
-        print(str(err))
-        usage()
-        sys.exit(1)
-    seed = time.time()
-    num_games = 1000
-    log_level = logging.DEBUG
-    global g_catch_exceptions
-    g_catch_exceptions = False
-    for o, a in opts:
-        if 0:
-            pass
-        elif o in ("-h", "--help"):
-            usage()
-            sys.exit()
-        elif o in ("--seed", ):
-            seed = a
-        elif o in ("--num-games", ):
-            num_games = int(a)
-        elif o in ("--log-level", ):
-            log_level = int(a)
-        elif o in ("--catch-exceptions", ):
-            g_catch_exceptions = 'off' != a
-        else:
-            raise Exception("unhandled option")
-    random.seed(seed)
+
+    c = argv[0]
 
     if 0:
         pass
 
-    elif 'human' == command:
-        logging.basicConfig(level=logging.INFO, format='%(message)s',
-                        stream=sys.stdout)
-        players = [make_player('human', 'p_human'), ]
-        if 0 == len(args):
-            players.append(make_player('computer', 'p_computer'))
-        else:
-            players.append(make_player(args[0], args[0]))
-        winner = play_game(players)
-        sys.exit()
+    elif 'new_tiles' == c:
+        tiles = new_tiles()
+        print tiles
 
-    elif 'game' == command:
-        logging.basicConfig(level=log_level, format='%(message)s',
-                        stream=sys.stdout)
-        players = []
-        for player_id, playername in enumerate(args):
-            player = make_player(chr(ord('a') + player_id), playername)
-            if None == player.play:
-                continue
-            players.append(player)
-        winner = play_game(players)
-        sys.exit()
+    elif 'wash_tiles' == c:
+        tiles = new_tiles()
+        wash_tiles(tiles)
+        print tiles
 
-    elif 'tournament' == command:
-        logging.basicConfig(level=log_level, format='%(message)s',
-                        stream=sys.stdout)
-        players = []
-        for player_id, playername in enumerate(args):
-            player = make_player(chr(ord('a') + player_id), playername)
-            if None == player.play:
-                continue
-            players.append(player)
-        play_tournament(num_games, players)
-        sys.exit()
+    elif 'count_tiles' == c:
+        tiles = new_tiles()
+        print len(tiles)
+
+    elif 'new_hand' == c:
+        tiles = new_tiles()
+        wash_tiles(tiles)
+        hand = tiles[:5]
+        hand.sort(reverse=True)
+        print serialize_hand(hand)
+
+    elif 'reserialize_hand' == c:
+        hand = deserialize_hand(argv[1])
+        print serialize_hand(hand)
+
+    elif 'new_hands' == c:
+        for i in range(argv[1]):
+            tiles = new_tiles()
+            wash_tiles(tiles)
+            hand = tiles[:5]
+            hand.sort(reverse=True)
+            print hand
+
+    elif 'deserialize' == c:
+        table = deserialize_table(argv[1])
+        print table
+
+    elif 'reserialize' == c:
+        table = deserialize_table(argv[1])
+        s = serialize_table(table)
+        print s
+
+    elif 'get_ends' == c:
+        table = deserialize_table(argv[1])
+        ends = get_ends(table)
+        print ends
+
+    elif 'get_count' == c:
+        table = deserialize_table(argv[1])
+        ends = get_ends(table)
+        count = get_count(ends)
+        print count
+
+    elif 'get_pip_ends' == c:
+        table = deserialize_table(argv[1])
+        ends = get_ends(table)
+        pip_ends = get_pip_ends(ends)
+        print pip_ends
+
+    elif 'get_plays' == c:
+        hand = deserialize_hand(argv[1])
+        table = deserialize_table(argv[2])
+        ends = get_ends(table)
+        pip_ends = get_pip_ends(ends)
+        plays = get_plays(ends, pip_ends, hand)
+        print plays
 
     else:
         print 'i don\'t know how to "%s".' % command
