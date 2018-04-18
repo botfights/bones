c0f986a6a95d7b32af38556f2fca31d992921c55
Colin M. Saunders <colins@surveymonkey.com>
2018-04-18 15:26:41 -0700

    use bones

--- p_computer/player.py
+++ p_computer/player.py
@@ -2,80 +2,18 @@
 
 import random
 
-
-def get_ends(table):
-    ends = {}
-    for i in table:
-        a, b = i
-
-        # first play?
-        #
-        if None == b:
-            ends[a] = 3
-
-        # play on a double?
-        #
-        elif b[0] == b[1]:
-            ends[b] = {3:2, 2:8, 8:4, 4:0}[ends[b]]
-            if a[0] == b[0]:
-                ends[a] = 2
-            else:
-                ends[a] = 1
-
-        # high on high
-        #
-        elif a[0] == b[0]:
-            ends[b] &= 14
-            ends[a] = 2
-
-        # high on low
-        #
-        elif a[0] == b[1]:
-            ends[b] &= 13
-            ends[a] = 2
-
-        # low on high
-        #
-        elif a[1] == b[0]:
-            ends[b] &= 14
-            ends[a] = 1
-
-        # low on low
-        #
-        elif a[1] == b[1]:
-            ends[b] &= 13
-            ends[a] = 1
-
-    for i, j in ends.items():
-        if 0 == j:
-            del ends[i]
-
-    return ends
-
-
-def get_count(ends):
-    count = 0
-    for end, playable in ends.items():
-        if end[0] == end[1]:
-            if playable & 3:
-                count += end[0] + end[1]
-        else:
-            if playable & 1:
-                count += end[0]
-            if playable & 2:
-                count += end[1]
-    return count
+import bones
 
 
 def get_play(whoami, my_hand, legal_plays, table, tile_counts, scores):
 
     # get the playable ends
     #
-    ends = get_ends(table)
+    ends = bones.get_ends(table)
 
     # get the count
     #
-    count = get_count(ends)
+    count = bones.get_count(ends)
 
     # first play? if so, find tiles that score
     #
